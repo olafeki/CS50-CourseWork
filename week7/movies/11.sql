818608bb615810c86965657ce5b3615c7413c1b1
olafeki <157868919+olafeki@users.noreply.github.com>
2024-01-27 08:30:14 +0200

    Adding coursework-files

---
+++ week7/movies/11.sql
@@ -0,0 +1,8 @@
+SELECT title
+FROM movies
+JOIN ratings ON movies.id = ratings.movie_id
+JOIN stars ON ratings.movie_id = stars.movie_id
+JOIN people ON people.id = stars.person_id
+WHERE people.name ='Chadwick Boseman'
+ORDER BY ratings.rating DESC
+LIMIT 5 ;
---
+++ week7/movies/13.sql
@@ -0,0 +1,11 @@
+SELECT DISTINCT(name)
+FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.title IN(
+SELECT DISTINCT(movies.title)
+FROM people
+JOIN stars ON stars.person_id = people.id
+JOIN movies ON stars.movie_id = movies.id
+WHERE people.name = 'Kevin Bacon' AND people.birth = '1958')
+ AND people.name != 'Kevin Bacon';
